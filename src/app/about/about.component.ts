bd0216a04c4b03388b0c17d03cbc7d4396655554
lilei <lilei@jiedaibao.com>
2018-03-20 11:17:43 +0800

    add life cycle

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,7 +9,36 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
+  ngOnChanges() {
+    console.log('About - ngOnChanges');
+  }
+
   ngOnInit() {
+    console.log('About - ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('About - ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('About - ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('About - ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('About - ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('About - ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('About - ngOnDestroy');
   }
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,7 +9,36 @@ export class ContactComponent implements OnInit {
 
   constructor() { }
 
+  ngOnChanges() {
+    console.log('Contact - ngOnChanges');
+  }
+
   ngOnInit() {
+    console.log('Contact - ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('Contact - ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('Contact - ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('Contact - ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('Contact - ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('Contact - ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('Contact - ngOnDestroy');
   }
 
 }
